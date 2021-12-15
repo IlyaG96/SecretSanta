a924e9d56bec4d7ba8e9927f0958b93d612f820f
IlyaG96 <ilyag_96@icloud.com>
2021-12-15 17:34:19 +1100

    bot can return two states: create game and take part in game

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import logging
 import os
+import time
 
+from dotenv import load_dotenv
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import (
     Updater,
@@ -27,9 +29,7 @@ IN_GAME = "in-game"
 
 def start(update, context):
 
-    bot = context.bot
-    url = helpers.create_deep_linked_url(bot.username, "params")
-    text = f"Организуй тайный обмен подарками, запусти праздничное настроение!:\n\n + {url}"
+    text = f"Организуй тайный обмен подарками, запусти праздничное настроение!"
     keyboard = [
         ["Создать игру"]
     ]
@@ -46,9 +46,12 @@ def start(update, context):
 
 def chose_game_name(update, context):
 
+    text = "Введите название игры"
+
     update.message.reply_text(
-        "Введите название игры",
-        reply_markup=ReplyKeyboardRemove)
+        text,
+        reply_markup=ReplyKeyboardRemove()
+    )
 
     return GAME_NAME
 
@@ -64,7 +67,8 @@ def chose_game_price(update, context):
             ["Без ограничения по стоимости"],
             ["До 500 рублей"],
             ["От 500 до 1000 рублей"],
-            ["От 1000 до 2000 рублей"]
+            ["От 1000 до 2000 рублей"],
+            ['Назад ⬅'],
         ]
         update.message.reply_text(
             text,
@@ -81,8 +85,7 @@ def chose_game_reg_ends(update, context):
 
     if update.message.text != 'Назад ⬅':
 
-        game_price = update.message.text
-        context.user_data['game_price'] = game_price
+        context.user_data['game_price'] = update.message.text
 
         game_name = context.user_data['game_name']
 
@@ -90,6 +93,7 @@ def chose_game_reg_ends(update, context):
         keyboard = [
             ["25.12.2021"],
             ["31.12.2021"],
+            ['Назад ⬅'],
         ]
         update.message.reply_text(
             text,
@@ -106,16 +110,10 @@ def chose_game_gift_date(update, context):
 
     if update.message.text != 'Назад ⬅':
 
-        game_reg_ends = update.message.text
-        context.user_data['game_reg_ends'] = game_reg_ends
+        context.user_data['game_reg_ends'] = update.message.text
+        keyboard = [['Назад ⬅']]
+        text = f"Введите день для отправки подарков в формате 29.12.2021:"
 
-        game_name = context.user_data['game_name']
-
-        text = f"Последний день регистрации в игре {game_name} это:"
-        keyboard = [
-            ["25.12.2021"],
-            ["31.12.2021"],
-        ]
         update.message.reply_text(
             text,
             reply_markup=ReplyKeyboardMarkup(
@@ -127,17 +125,65 @@ def chose_game_gift_date(update, context):
     return GAME_GIFT_DATE
 
 
+def game_confirmation(update, context):
+    if update.message.text != 'Назад ⬅':
+
+        context.user_data['game_gift_send'] = update.message.text
+
+        game_name = context.user_data['game_name']
+        game_price = context.user_data['game_price']
+        game_reg_ends = context.user_data['game_reg_ends']
+        game_gift_date = context.user_data['game_gift_send']
+
+        keyboard = [['Назад ⬅'], ["Подтвердить"]]
+
+        text = f"Подтвердите детали игры:\n" \
+               f"Название игры: {game_name} \n" \
+               f"Ограничение по стоимости: {game_price} \n" \
+               f"Последний день для регистрации: {game_reg_ends} \n" \
+               f"День для отправки подарков: {game_gift_date} \n"
+
+        update.message.reply_text(
+            text,
+            reply_markup=ReplyKeyboardMarkup(
+                keyboard,
+                resize_keyboard=True,
+            )
+        )
+
+        return GAME_CONFIRMATION
+
+
+def send_game_url(update, context):
+
+    bot = context.bot
+    game_id = "params"
+    url = helpers.create_deep_linked_url(bot.username, game_id)
+
+    text = f"Ссылка для участия в игре: {url}"
+    update.message.reply_text(
+        text,
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+
 if __name__ == '__main__':
+
     load_dotenv()
     TG_TOKEN = os.getenv("TG_TOKEN")
     updater = Updater(token=TG_TOKEN)
     dispatcher = updater.dispatcher
 
     conv_handler = ConversationHandler(
-        entry_points=[MessageHandler(Filters.regex('^Начать$'), start), CommandHandler("start", start)],
+        entry_points=[
+            CommandHandler("start", start_santa_game, filters=Filters.regex('params')),
+            CommandHandler("start", start)
+            ],
+
         states={
             SANTA_GAME: [
-                CommandHandler("start", start_santa_game, filters=Filters.regex('params')),
+                CommandHandler("start", start_santa_game,
+                               filters=Filters.regex('params')),
                 MessageHandler(Filters.regex('^Создать игру$'), chose_game_name),
             ],
             GAME_NAME: [
@@ -150,12 +196,19 @@ if __name__ == '__main__':
             GAME_REG_ENDS: [
                 MessageHandler(Filters.regex('^Назад ⬅$'), chose_game_price),
                 MessageHandler(Filters.text, chose_game_gift_date)
+            ],
+            GAME_GIFT_DATE: [
+                MessageHandler(Filters.regex('^Назад ⬅$'), chose_game_reg_ends),
+                MessageHandler(Filters.text, game_confirmation)
+            ],
+            GAME_CONFIRMATION: [
+                MessageHandler(Filters.regex('^Назад ⬅$'), chose_game_gift_date),
+                MessageHandler(Filters.regex('^Подтвердить$'), send_game_url)
             ]
-
 
         },
         fallbacks=[CommandHandler('start', start), MessageHandler(Filters.regex('^Начать$'), start)],
-        per_user=False,
+        per_user=True,
         per_chat=True
     )
     dispatcher.add_handler(conv_handler)
--- santa_game.py
+++ santa_game.py
@@ -1,3 +1,5 @@
+import logging
+
 from telegram import ReplyKeyboardMarkup
 
 
