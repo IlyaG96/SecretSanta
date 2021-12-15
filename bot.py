d1cc7dc041e6e6e39a2c38b4b8a4fb89cb5fed83
IlyaG96 <ilyag_96@icloud.com>
2021-12-15 17:57:20 +1100

    fix double brackets

--- bot.py
+++ bot.py
@@ -17,21 +17,21 @@ from telegram.utils import helpers
 from santa_game import start_santa_game
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
 
 GAME_NAME, GAME_PRICE, GAME_REG_ENDS, GAME_BUILD, GAME_GIFT_DATE, GAME_CONFIRMATION = range(6)
-SANTA_GAME = "share-link-with-game-id"
-IN_GAME = "in-game"
+SANTA_GAME = 'share-link-with-game-id'
+IN_GAME = 'in-game'
 
 
 def start(update, context):
 
-    text = f"Организуй тайный обмен подарками, запусти праздничное настроение!"
+    text = 'Организуй тайный обмен подарками, запусти праздничное настроение!'
     keyboard = [
-        ["Создать игру"]
+        ['Создать игру']
     ]
     update.message.reply_text(
         text,
@@ -46,7 +46,7 @@ def start(update, context):
 
 def chose_game_name(update, context):
 
-    text = "Введите название игры"
+    text = 'Введите название игры'
 
     update.message.reply_text(
         text,
@@ -64,10 +64,10 @@ def chose_game_price(update, context):
         context.user_data['game_name'] = game_name
         text = f"Для игры {game_name} будет ограничение по стоимости?"
         keyboard = [
-            ["Без ограничения по стоимости"],
-            ["До 500 рублей"],
-            ["От 500 до 1000 рублей"],
-            ["От 1000 до 2000 рублей"],
+            ['Без ограничения по стоимости'],
+            ['До 500 рублей'],
+            ['От 500 до 1000 рублей'],
+            ['От 1000 до 2000 рублей'],
             ['Назад ⬅'],
         ]
         update.message.reply_text(
@@ -89,10 +89,10 @@ def chose_game_reg_ends(update, context):
 
         game_name = context.user_data['game_name']
 
-        text = f"Последний день регистрации в игре {game_name} это:"
+        text = f'Последний день регистрации в игре {game_name} это:'
         keyboard = [
-            ["25.12.2021"],
-            ["31.12.2021"],
+            ['25.12.2021'],
+            ['31.12.2021'],
             ['Назад ⬅'],
         ]
         update.message.reply_text(
@@ -112,7 +112,7 @@ def chose_game_gift_date(update, context):
 
         context.user_data['game_reg_ends'] = update.message.text
         keyboard = [['Назад ⬅']]
-        text = f"Введите день для отправки подарков в формате 29.12.2021:"
+        text = 'Введите день для отправки подарков в формате 29.12.2021:'
 
         update.message.reply_text(
             text,
@@ -135,13 +135,13 @@ def game_confirmation(update, context):
         game_reg_ends = context.user_data['game_reg_ends']
         game_gift_date = context.user_data['game_gift_send']
 
-        keyboard = [['Назад ⬅'], ["Подтвердить"]]
+        keyboard = [['Назад ⬅'], ['Подтвердить']]
 
-        text = f"Подтвердите детали игры:\n" \
-               f"Название игры: {game_name} \n" \
-               f"Ограничение по стоимости: {game_price} \n" \
-               f"Последний день для регистрации: {game_reg_ends} \n" \
-               f"День для отправки подарков: {game_gift_date} \n"
+        text = 'Подтвердите детали игры:\n' \
+               f'Название игры: {game_name} \n' \
+               f'Ограничение по стоимости: {game_price} \n' \
+               f'Последний день для регистрации: {game_reg_ends} \n' \
+               f'День для отправки подарков: {game_gift_date} \n'
 
         update.message.reply_text(
             text,
@@ -157,10 +157,10 @@ def game_confirmation(update, context):
 def send_game_url(update, context):
 
     bot = context.bot
-    game_id = "params"
+    game_id = 'params'
     url = helpers.create_deep_linked_url(bot.username, game_id)
 
-    text = f"Ссылка для участия в игре: {url}"
+    text = f'Ссылка для участия в игре: {url}'
     update.message.reply_text(
         text,
         reply_markup=ReplyKeyboardRemove()
@@ -170,19 +170,19 @@ def send_game_url(update, context):
 if __name__ == '__main__':
 
     load_dotenv()
-    TG_TOKEN = os.getenv("TG_TOKEN")
+    TG_TOKEN = os.getenv('TG_TOKEN')
     updater = Updater(token=TG_TOKEN)
     dispatcher = updater.dispatcher
 
     conv_handler = ConversationHandler(
         entry_points=[
-            CommandHandler("start", start_santa_game, filters=Filters.regex('params')),
-            CommandHandler("start", start)
+            CommandHandler('start', start_santa_game, filters=Filters.regex('params')),
+            CommandHandler('start', start)
             ],
 
         states={
             SANTA_GAME: [
-                CommandHandler("start", start_santa_game,
+                CommandHandler('start', start_santa_game,
                                filters=Filters.regex('params')),
                 MessageHandler(Filters.regex('^Создать игру$'), chose_game_name),
             ],
