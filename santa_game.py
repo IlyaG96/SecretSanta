d4d8c8b87fd34753f997fe56ac8833fa7f3a687c
IlyaG96 <ilyag_96@icloud.com>
2021-12-16 11:57:02 +1100

    fix game owner mistake

--- santa_game.py
+++ santa_game.py
@@ -5,9 +5,10 @@ def start_santa_game(update, context):
     game_name = context.args[0]
     user = update.message.from_user
     first_name = user.first_name
+
     with open(file=f"{game_name}.txt", mode="r") as file:
         text = file.read()
-        game_owner = text.split("owner")[1]
+        game_owner = text.split("owner")[1].strip()
 
     if first_name == game_owner:
         keyboard = [
