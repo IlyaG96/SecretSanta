df04a026a51f219ce11882a370c92949a79ae67f
IlyaG96 <ilyag_96@icloud.com>
2022-03-20 15:13:00 +1100

    change command to pull

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -16,7 +16,7 @@ def deploy():
 
 def _get_latest_source():
     if exists('.git'):
-        run('git fetch')
+        run('git pull')
     else:
         run(f'git clone {REPO_URL} .')
     current_commit = local("git log -n 1 --format=%H", capture=True)
